972f1814532690ed8dcdd9e84a9cb9a783ba400e
FBI1337 <84200204+FBI1337@users.noreply.github.com>
2024-10-22 18:43:48 +0300

    fix

---
+++ Snowflake.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FNA_Snowrain_Shelkynov
+{
+    internal class Snowflake
+    {
+        public Vector2 position;
+        public readonly float speed;
+        public readonly float size;
+        public readonly Texture2D texture;
+        /// <summary>
+        /// Конструктор снежинки. Инициализирует скорость, размер, позицию и текстуру
+        /// </summary>
+        public Snowflake(Vector2 position, float speed, float size, Texture2D texture)
+        {
+            this.position = position;
+            this.speed = speed;
+            this.size = size;
+            this.texture = texture;
+        }
+        /// <summary>
+        /// Пдание снежинки
+        /// </summary>
+        public void Fall(GameTime gameTime)
+        {
+            position.Y += speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+        /// <summary>
+        /// Отрисовка снежинки
+        /// </summary>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, null, Color.White, 0f, Vector2.Zero, size, SpriteEffects.None, 0f);
+        }
+    }
+}
---
+++ Snowrain.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+namespace FNA_Snowrain_Shelkynov
+{
+    public class Snowrain : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        private Texture2D spriteTexture;
+        private List<Snowflake> sprite;
+        private readonly Random random = new Random();
+        private const int WindowHeight = 1920;
+        private const int WindowWidth = 1080;
+        /// <summary>
+        /// Это конструктор снегопада. Инициализирует все параметры графики
+        /// </summary>
+        public Snowrain()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = 1920;
+            graphics.PreferredBackBufferHeight = 1080;
+            graphics.IsFullScreen = true;
+            graphics.ApplyChanges();
+
+        }
+        /// <summary>
+        /// Инициализация игры. Здесь добавляется логика
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+        /// <summary>
+        /// Загрузка контента.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            spriteTexture = Content.Load<Texture2D>("Snowflake");
+            sprite = new List<Snowflake>();
+
+            base.LoadContent();
+        }
+    }
+}
